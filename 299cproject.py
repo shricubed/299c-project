f989b04ec3ef32903b15c7ee467ce5dbbca0a9d6
shricubed <ishrikar@live.com>
2022-05-08 11:02:55 -0400

    Update 299cproject.py

--- 299cproject.py
+++ 299cproject.py
@@ -1,31 +1,25 @@
 from __future__ import division
 import math
-
-from pyaudio import PyAudio # sudo apt-get install python{,3}-pyaudio
+from pyaudio import PyAudio 
 
 try:
     from itertools import izip
-except ImportError: # Python 3
+except ImportError:
     izip = zip
     xrange = range
 
-def sine_tone(frequency, duration, volume=1, sample_rate=22050):
+def playNote(frequency, duration, volume=1, sample_rate=22050):
     n_samples = int(sample_rate * duration)
     restframes = n_samples % sample_rate
 
     p = PyAudio()
-    stream = p.open(format=p.get_format_from_width(1), # 8bit
-                    channels=1, # mono
-                    rate=sample_rate,
-                    output=True)
+    stream = p.open(format=p.get_format_from_width(1), channels=1, rate=sample_rate, output=True)
+                
     s = lambda t: volume * math.sin(2 * math.pi * frequency * t / sample_rate)
     samples = (int(s(t) * 0x7f + 0x80) for t in xrange(n_samples))
-    for buf in izip(*[samples]*sample_rate): # write several samples at a time
+    for buf in izip(*[samples]*sample_rate):
         stream.write(bytes(bytearray(buf)))
-
-    # fill remainder of frameset with silence
     stream.write(b'\x80' * restframes)
-
     stream.stop_stream()
     stream.close()
     p.terminate()
